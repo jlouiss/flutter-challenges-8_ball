8a42f2b77b0a474da1e198de5873157fa44140ac
Jean Louis Borba Rosa Salbego <info@jlouis.me>
2020-08-03 18:46:47 +0100

    :sparkles: Scaffold BallPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(EightBallApp());
+}
+
+class EightBallApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: BallPage());
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey.shade900,
+        title: Text('Ask me anything'),
+      ),
+      body: Container(
+        color: Colors.blueGrey.shade300,
+      ),
+    );
+  }
+}
